cfe024b82c61b4c0983de51279dc231b96013bba
Ayush Kaushik <anon.programmer305@gmail.com>
2025-09-29 19:43:03 -0400

    Tracking events with analytics in AboutMe section

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { UserContext } from "../context/UserContext";
+import { AnalyticsContext } from "../context/AnalyticsContext";
 import Emoji from "../components/Emoji";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithubSquare, faLinkedin, faDev } from "@fortawesome/free-brands-svg-icons";
+import { logEvent } from 'firebase/analytics';
 
 const Aboutme = () => {
     const userContext = useContext(UserContext);
+    const { analytics } = useContext(AnalyticsContext);
+
+    const handleExternalLinkClick = (platform) => {
+        if (analytics) {
+            logEvent(analytics, 'external_link_click', {
+                platform: platform
+            });
+        }
+    };
 
     return (
         <div>
@@ -13,15 +24,15 @@ const Aboutme = () => {
                 className={"aboutme-container"}
             >
                 <span>
-                    <h1>{"Hi! I'm Ayush Kaushik"}<Emoji symbol={"👋"} label={"wave"} className={"about-me-wave-icon"}/></h1>
+                    <h1>{"Hi! I'm Ayush Kaushik"}<Emoji symbol={"👋"} label={"wave"} className={"about-me-wave-icon"} /></h1>
                     <h2>{"I'm a Software Engineer @ OpenText."}</h2>
                     <div>
-                        <a href={userContext.userInfo.html_url}><FontAwesomeIcon className={"project-icon"}
+                        <a href={userContext.userInfo.html_url} onClick={() => handleExternalLinkClick('GitHub')}><FontAwesomeIcon className={"project-icon"}
                             icon={faGithubSquare} /></a>
-                        <a href={"https://www.linkedin.com/in/ayushkaushik"}><FontAwesomeIcon
+                        <a href={"https://www.linkedin.com/in/ayushkaushik"} onClick={() => handleExternalLinkClick('LinkedIn')}><FontAwesomeIcon
                             className={"project-icon"}
                             icon={faLinkedin} /></a>
-                        <a href={"https://dev.to/ayushkaushik"}><FontAwesomeIcon className={"project-icon"}
+                        <a href={"https://dev.to/ayushkaushik"} onClick={() => handleExternalLinkClick('Dev.to')}><FontAwesomeIcon className={"project-icon"}
                             icon={faDev} /></a>
                     </div>
                 </span>
