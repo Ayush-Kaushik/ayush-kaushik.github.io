1ec2e401200b872f444e787e488db9a1792fb003
Ayush Kaushik <anon.programmer305@gmail.com>
2025-12-16 11:16:24 -0500

    Remove Firebase and update analytics integration
    - Firebase and related dependencies were removed from the project.
    - The analytics utility and its usage in App.js were refactored to use a custom solution instead of Firebase Analytics.
    - package.json and public/index.html were updated to reflect these changes.

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,5 +1,11 @@
 export const sendGAEvent = (eventCategory, eventAction, eventLabel = null, eventValue = null) => {
+
+    console.log(window.gtag);
+
   if (typeof window !== 'undefined' && window.gtag) {
+
+    console.log("Sending GA Event:", eventCategory, eventAction, eventLabel, eventValue);
+
     window.gtag('event', eventAction, {
       'event_category': eventCategory,
       ...(eventLabel && { 'event_label': eventLabel }),
